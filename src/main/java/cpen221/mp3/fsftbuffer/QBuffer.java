9d603a2ae82406f5bd311c6969da8bdad897d9e9
Phoenix Liu <55338489+phoenixthefirebird@users.noreply.github.com>
2020-12-15 22:47:50 -0800

    implemented QBuffer class to store the queries, please review the documentations

---
+++ src/main/java/cpen221/mp3/fsftbuffer/QBuffer.java
@@ -0,0 +1,39 @@
+package cpen221.mp3.fsftbuffer;
+
+/**
+ * this immutable Bufferable class stores the query Strings used in WikiMediator
+ */
+public class QBuffer implements Bufferable{
+    /**
+     * AF:
+     * stores a String from query request
+     *
+     * RI:
+     * query is not null
+     * query never changes (immutable)
+     * the id is unique from all other instances of QBuffer
+     */
+    private final String query;
+
+    public QBuffer(String search){
+        this.query = search;
+    }
+
+    /**
+     * get the id of this QBuffer object
+     * @return String representing the id of this object
+     * */
+    @Override
+    public String id() {
+        return ((Integer) this.hashCode()).toString();
+    }
+
+
+    /**
+     * get the stored query in String
+     * @return a String representing the query
+     * */
+    public String getQuery() {
+        return query;
+    }
+}
