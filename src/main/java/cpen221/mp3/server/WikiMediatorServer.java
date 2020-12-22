a8efafc1774e9f627f797df9e2ee04defbfd2b6d
Han Cho <freetyhan@gmail.com>
2020-12-21 20:17:54 -0800

    AF querylog updated

--- src/main/java/cpen221/mp3/server/WikiMediatorServer.java
+++ src/main/java/cpen221/mp3/server/WikiMediatorServer.java
@@ -82,47 +82,47 @@ public class WikiMediatorServer {
      *             if connection encounters an error
      */
     private void handle(Socket socket) throws IOException {
-        System.err.println("client connected");
-
-        // get the socket's input stream, and wrap converters around it
-        // that convert it from a byte stream to a character stream,
-        // and that buffer it so that we can read a line at a time
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                socket.getInputStream()));
-
-        // similarly, wrap character=>bytestream converter around the
-        // socket output stream, and wrap a PrintWriter around that so
-        // that we have more convenient ways to write Java primitive
-        // types to it.
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(
-                socket.getOutputStream()), true);
-
-        try {
-            // each request is a single line containing a number
-            for (String line = in.readLine(); line != null; line = in
-                    .readLine()) {
-                System.err.println("request: " + line);
-                try {
-                    int x = Integer.valueOf(line);
-                    // compute answer and send back to client
-                    BigInteger y = fibonacci(x);
-                    System.err.println("reply: " + y);
-                    out.println(y);
-                } catch (NumberFormatException e) {
-                    // complain about ill-formatted request
-                    System.err.println("reply: err");
-                    out.print("err\n");
-                }
-                //TODO: to read request
-
-                // important! our PrintWriter is auto-flushing, but if it were
-                // not:
-                // out.flush();
-            }
-        } finally {
-            out.close();
-            in.close();
-        }
+//        System.err.println("client connected");
+//
+//        // get the socket's input stream, and wrap converters around it
+//        // that convert it from a byte stream to a character stream,
+//        // and that buffer it so that we can read a line at a time
+//        BufferedReader in = new BufferedReader(new InputStreamReader(
+//                socket.getInputStream()));
+//
+//        // similarly, wrap character=>bytestream converter around the
+//        // socket output stream, and wrap a PrintWriter around that so
+//        // that we have more convenient ways to write Java primitive
+//        // types to it.
+//        PrintWriter out = new PrintWriter(new OutputStreamWriter(
+//                socket.getOutputStream()), true);
+//
+//        try {
+//            // each request is a single line containing a number
+//            for (String line = in.readLine(); line != null; line = in
+//                    .readLine()) {
+//                System.err.println("request: " + line);
+//                try {
+//                    int x = Integer.valueOf(line);
+//                    // compute answer and send back to client
+//                    BigInteger y = fibonacci(x);
+//                    System.err.println("reply: " + y);
+//                    out.println(y);
+//                } catch (NumberFormatException e) {
+//                    // complain about ill-formatted request
+//                    System.err.println("reply: err");
+//                    out.print("err\n");
+//                }
+//                //TODO: to read request
+//
+//                // important! our PrintWriter is auto-flushing, but if it were
+//                // not:
+//                // out.flush();
+//            }
+//        } finally {
+//            out.close();
+//            in.close();
+//        }
     }
 
 }
