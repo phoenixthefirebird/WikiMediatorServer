190cbce4ebba921656674674e38c8d8c74063d06
Han Cho <freetyhan@gmail.com>
2020-12-22 21:02:54 -0800

    mediator server test case

---
+++ src/test/java/cpen221/mp3/WikiMediatorServerTests.java
@@ -0,0 +1,15 @@
+package cpen221.mp3;
+
+import cpen221.mp3.server.WikiMediatorServer;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class WikiMediatorServerTests {
+    @Test
+    public void test1() throws IOException {
+        WikiMediatorServer server = new WikiMediatorServer(2, 5);
+        server.serve();
+
+    }
+}
